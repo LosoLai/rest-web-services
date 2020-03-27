178e0a79edbd571886b67b050644b271b2e5f63c
LosoLai <losolai@gmail.com>
2020-03-27 17:09:21 +1100

    Complete 3.31 - internationalization create multiple properties files to have various language version of message

--- src/main/java/com/in28Minutes/rest/webservices/restwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28Minutes/rest/webservices/restwebservices/helloworld/HelloWorldController.java
@@ -1,9 +1,16 @@
 package com.in28Minutes.rest.webservices.restwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @RequestMapping(method=RequestMethod.GET, path="/hello-world")
     public String helloWorld(){
@@ -18,5 +25,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean/path/{name}")
     public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale){
+        return messageSource.getMessage("good.morning.message", null, locale);
     }
 }
