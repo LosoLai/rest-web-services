e80c026cad6f5ea3b8e55704c7233f69f5bd7000
LosoLai <losolai@gmail.com>
2020-04-02 16:24:14 +1100

    Complete 3.32_1 - internationalization get rid of request header para

--- src/main/java/com/in28Minutes/rest/webservices/restwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28Minutes/rest/webservices/restwebservices/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.in28Minutes.rest.webservices.restwebservices.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
@@ -28,7 +29,7 @@ public class HelloWorldController {
     }
 
     @GetMapping("/hello-world-internationalized")
-    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale){
-        return messageSource.getMessage("good.morning.message", null, locale);
+    public String helloWorldInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
