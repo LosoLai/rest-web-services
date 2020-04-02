6b2330613248b0b531236e0a72c095677c4cfed3
LosoLai <losolai@gmail.com>
2020-04-02 18:05:11 +1100

    Complete 3.37 - enhancing swagger documentation

--- src/main/java/com/in28Minutes/rest/webservices/restwebservices/user/User.java
+++ src/main/java/com/in28Minutes/rest/webservices/restwebservices/user/User.java
@@ -1,18 +1,24 @@
 package com.in28Minutes.rest.webservices.restwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@ApiModel(description = "All details about User.")
 public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name should be longer than 2 characters.")
+    @ApiModelProperty(notes = "Name should be at least 2 characters.")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "Birthday should be in the past.")
     private Date birthDate;
     private List<Post> posts;
 
