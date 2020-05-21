2ddb5e4a32c3e263a1f253be2be3a4c51b84dd35
LosoLai <losolai@gmail.com>
2020-05-21 16:05:38 +1000

    Complete 3.41 - create person versioning controller

---
+++ src/main/java/com/in28Minutes/rest/webservices/restwebservices/versioning/PersonVersioningController.java
@@ -0,0 +1,18 @@
+package com.in28Minutes.rest.webservices.restwebservices.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonVersioningController {
+
+    @GetMapping("v1/person")
+    public PersonV1 getPersonV1(){
+        return new PersonV1("Bob Charlies");
+    }
+
+    @GetMapping("v2/person")
+    public PersonV2 getPersonV2(){
+        return new PersonV2(new Name("Bob", "Charlies"));
+    }
+}
