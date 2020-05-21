6e6e05edf6d7731fdef392ed34a9388db8d38416
LosoLai <losolai@gmail.com>
2020-05-21 16:13:58 +1000

    Complete 3.42.1 - way2: use params to specify the version

--- src/main/java/com/in28Minutes/rest/webservices/restwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/in28Minutes/rest/webservices/restwebservices/versioning/PersonVersioningController.java
@@ -6,13 +6,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
-    @GetMapping("v1/person")
+    @GetMapping("/v1/person")
     public PersonV1 getPersonV1(){
         return new PersonV1("Bob Charlies");
     }
 
-    @GetMapping("v2/person")
+    @GetMapping("/v2/person")
     public PersonV2 getPersonV2(){
+        return new PersonV2(new Name("Bob", "Charlies"));
+    }
+
+    @GetMapping(value="/person/param", params="version=1")
+    public PersonV1 getParamV1(){
+        return new PersonV1("Bob Charlies");
+    }
+
+    @GetMapping(value="/person/param", params="version=2")
+    public PersonV2 getParamV2(){
         return new PersonV2(new Name("Bob", "Charlies"));
     }
 }
